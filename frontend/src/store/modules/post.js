0d072206a574695b72190a8c9a14246240790aef
WOOJE <woojenoh@gmail.com>
2018-12-04 21:41:09 +0900

    Redux, Route Setting

---
+++ frontend/src/store/modules/post.js
@@ -0,0 +1,13 @@
+import { createAction, handleActions } from "redux-actions";
+import { Map } from "immutable";
+import { pender } from "redux-pender";
+
+// action types
+
+// action creators
+
+// initial state
+const initialState = Map({});
+
+// reducer
+export default handleActions({}, initialState);
